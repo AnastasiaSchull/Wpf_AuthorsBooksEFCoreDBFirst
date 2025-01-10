3a13b5814a205ab7ed8a099c4f2ee6b8e36451c0
Nastia <nastasia.schull@gmail.com>
2025-01-11 00:59:03 +0100

    Implemented full CRUD functionality for Authors and Books

---
+++ Wpf_AuthorsBooksEFCoreDBFirst/Views/InputAuthorWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf_AuthorsBooksEFCoreDBFirst.Views
+{
+    /// <summary>
+    /// Interaction logic for InputAuthorWindow.xaml
+    /// </summary>
+    public partial class InputAuthorWindow : Window
+    {
+        public string AuthorName
+        {
+            get => AuthorNameTextBox.Text;
+            set => AuthorNameTextBox.Text = value;
+        }
+        public InputAuthorWindow()
+        {
+            InitializeComponent();
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            this.ResizeMode = ResizeMode.NoResize;
+        }
+        public InputAuthorWindow(string existingAuthorName) : this()
+        {
+            AuthorName = existingAuthorName;
+        }
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            AuthorName = AuthorNameTextBox.Text;
+            if (string.IsNullOrWhiteSpace(AuthorName))
+            {
+                MessageBox.Show("Please enter a valid author name!");
+                return;
+            }
+            DialogResult = true;  // закрываем окно с положительным результатом
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;  // закрываем окно с отрицательным результатом
+        }
+    }
+}
---
+++ Wpf_AuthorsBooksEFCoreDBFirst/Views/InputBookWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace Wpf_AuthorsBooksEFCoreDBFirst.Views
+{
+    /// <summary>
+    /// Interaction logic for InputBookWindow.xaml
+    /// </summary>
+    public partial class InputBookWindow : Window
+    {
+        public string BookTitle { get; private set; }
+
+        public InputBookWindow()
+        {
+            InitializeComponent();
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            this.ResizeMode = ResizeMode.NoResize;
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            BookTitle = BookTitleTextBox.Text;
+            DialogResult = true;  
+            Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false; 
+            Close();
+        }
+    }
+}
